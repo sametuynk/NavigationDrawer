3c035dc1e5438442d14957e1a8716cfb2298aa13
samet.uyanik <samet.uyanik2756@gmail.com>
2024-01-30 15:13:16 +0300

    navigationView,navHostFragment,toolbar

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.navigationdrawer;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.navigationdrawer.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        //navigationView ile navHostFragment birleştime işlemi yaptım.
+        NavigationUI.setupWithNavController(binding.navigationView,navHostFragment.getNavController());
+
+        binding.toolbar.setTitle("Başlık");
+
+        //drawer ile toolbarı beraber çalışır hale getirildi.
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,binding.drawer,binding.toolbar,0,0);
+        binding.drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
     }
 }
